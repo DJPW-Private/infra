a0fd7837669615fd1904399e3a05bbef5cdd0fc1
DJPW-Private <darren+github@warta.org>
2025-07-10 15:49:00 -0400

    Commented out slack_handler module.

--- _REGIONS/us-east-2/_main.tf
+++ _REGIONS/us-east-2/_main.tf
@@ -1,7 +1,7 @@
-module "slack_handler" {
-    source = "../../projects/slack_handler"
-    global = local.global
-}
+# module "slack_handler" {
+#     source = "../../projects/slack_handler"
+#     global = local.global
+# }
 
 module "route53" {
     source = "../../modules_legacy/Route53"
